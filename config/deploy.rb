2d22d756f90e4c8c59bae1997e6294fe2fbe14e7
Kumar <kumarks1122@gmail.com>
2017-11-13 04:02:46 +0530

    Fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 lock "~> 3.10.0"
 
 set :application, "diagnal"
-set :repo_url, "git@github.com:pSenthil202/terminal2.git"
+set :repo_url, "git@bitbucket.org:kumarks1122/diagnal-workshop.git"
 set :scm, :git
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/terminal2"
+set :deploy_to, "/var/www/diagnal"
 
 # Default value for :format is :airbrussh.
 set :format, :pretty
@@ -24,21 +24,17 @@ set :pty, true
 
 set :rvm_map_bins, %w{rake gem bundle ruby rails}
 
-# Default value for :linked_files is []
-append :linked_files, "config/database.yml"
-
 # Default value for linked_dirs is []
 append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/cache", "public/uploads", "vendor/bundle", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-set :stages, %w(staging production)
-set :default_stage, "staging"
+set :stages, %w(development staging production)
+set :default_stage, "development"
 set :ssh_options, {:forward_agent => true,
 	 :user=> "root",
    :keepalive => true,
    :keepalive_interval => 3000}
 set :user, "root"
-set :migration_role, 'app' # Defaults to 'db'
 set :assets_roles, [:app] # Defaults to [:web]
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
